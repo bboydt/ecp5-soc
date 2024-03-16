c630ae7568d04b184e094fb7e648375e341c507d
Boyd Thomas <boydbt48@gmail.com>
2024-03-16 00:40:15 -0400

    BRAM now handles wb_sel properly

--- rtl/ecp5_soc/cores/bram.v
+++ rtl/ecp5_soc/cores/bram.v
@@ -1,4 +1,4 @@
-// BRAM
+/// BRAM
 //
 // Make sure LENGTH is a multiple of 16KB
 
@@ -21,12 +21,14 @@ module bram #(
         if (wb_cyc & wb_stb) begin
             if (~wb_ack) begin
                 if (wb_we) begin
-                    {
-                        data[MIN_ADDR_WIDTH'(wb_adr[ADDR_WIDTH-1:2])][7:0],
-                        data[MIN_ADDR_WIDTH'(wb_adr[ADDR_WIDTH-1:2])][15:8],
-                        data[MIN_ADDR_WIDTH'(wb_adr[ADDR_WIDTH-1:2])][23:16],
-                        data[MIN_ADDR_WIDTH'(wb_adr[ADDR_WIDTH-1:2])][31:24]
-                    } <= wb_mosi;
+                    if (wb_sel[3])
+                        data[MIN_ADDR_WIDTH'(wb_adr[ADDR_WIDTH-1:2])][0*8+:8] <= wb_mosi[3*8+:8];
+                    if (wb_sel[2])
+                        data[MIN_ADDR_WIDTH'(wb_adr[ADDR_WIDTH-1:2])][1*8+:8] <= wb_mosi[2*8+:8];
+                    if (wb_sel[1])
+                        data[MIN_ADDR_WIDTH'(wb_adr[ADDR_WIDTH-1:2])][2*8+:8] <= wb_mosi[1*8+:8];
+                    if (wb_sel[0])
+                        data[MIN_ADDR_WIDTH'(wb_adr[ADDR_WIDTH-1:2])][3*8+:8] <= wb_mosi[0*8+:8];
                     wb_ack <= 1;
                     wb_err <= 0;
                 end else begin
@@ -47,4 +49,3 @@ module bram #(
     end
 
 endmodule
-
